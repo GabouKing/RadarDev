83934f71e76bfb4362fdc156a7ee165cdb59a16c
Gabriel Ribeiro <gribeirodelimas2@gmail.com>
2020-01-25 01:11:33 -0300

    RadarDev

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL:'http://172.23.230.69:3333'
+    baseURL:'http://10.0.0.102:3333'
     //'http://6x-ryi.anonymous.mobile.exp.direct:80',
 });
 export default api;
